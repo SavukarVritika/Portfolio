2e6bb0c41ea44a97d5b28d1272d2e3dffe881faf
SavukarVritika <savukarvritika@gmail.com>
2025-07-17 15:10:59 +0530

    Remove backend, convert to static site for Vercel deployment

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function wrapper for your Express app
-import app from './server/index';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  app(req, res);
-}
