45fb4a8f3220f5a67497fd9e95d8e3ee89b47933
SavukarVritika <savukarvritika@gmail.com>
2025-07-17 14:49:23 +0530

    Move server directory into api/ for Vercel compatibility and update import

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 // Vercel serverless function wrapper for your Express app
-import app from '../server/index';
+import app from './server/index';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
